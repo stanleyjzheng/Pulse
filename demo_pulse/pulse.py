c04a27b3bfdc73d54df4f0476d5dc5a0523e204d
Gregory Tarr <greg@tarr.ie>
2021-01-16 19:55:56 +0000

    added plot code

--- demo_pulse/pulse.py
+++ demo_pulse/pulse.py
@@ -37,6 +37,92 @@ class PulseMonitor(object):
         self.find_faces = True
         self.data_buffer, self.times = [], []
 
+
+    def plotXY(self, data, size=(280,640), margin=25, name="data", labels=[], skip=[],
+            showmax=[], bg=None, label_ndigits=[], showmax_digits=[]):
+        
+        for x, y in data:
+            if len(x) < 2 or len(y) < 2:
+                return
+        
+        n_plots = len(data)
+        w = float(size[1])
+        h = size[0]/float(n_plots)
+        
+        z = np.zeros((size[0],size[1],3))
+        
+        if isinstance(bg,np.ndarray):
+            wd = int(bg.shape[1]/bg.shape[0]*h )
+            bg = cv2.resize(bg,(wd,int(h)))
+            if len(bg.shape) == 3:
+                r = combine(bg[:,:,0],z[:,:,0])
+                g = combine(bg[:,:,1],z[:,:,1])
+                b = combine(bg[:,:,2],z[:,:,2])
+            else:
+                r = combine(bg,z[:,:,0])
+                g = combine(bg,z[:,:,1])
+                b = combine(bg,z[:,:,2])
+            z = cv2.merge([r,g,b])[:,:-wd,]    
+        
+        i = 0
+        P = []
+        for x,y in data:
+            x = np.array(x)
+            y = -np.array(y)
+            
+            xx = (w-2*margin)*(x - x.min()) / (x.max() - x.min())+margin
+            yy = (h-2*margin)*(y - y.min()) / (y.max() - y.min())+margin + i*h
+            mx = max(yy)
+            if labels:
+                if labels[i]:
+                    for ii in range(len(x)):
+                        if ii%skip[i] == 0:
+                            col = (255,255,255)
+                            ss = '{0:.%sf}' % label_ndigits[i]
+                            ss = ss.format(x[ii]) 
+                            cv2.putText(z,ss,(int(xx[ii]),int((i+1)*h)),
+                                        cv2.FONT_HERSHEY_PLAIN,1,col)           
+            if showmax:
+                if showmax[i]:
+                    col = (0,255,0)    
+                    ii = np.argmax(-y)
+                    ss = '{0:.%sf} %s' % (showmax_digits[i], showmax[i])
+                    ss = ss.format(x[ii]) 
+                    #"%0.0f %s" % (x[ii], showmax[i])
+                    cv2.putText(z,ss,(int(xx[ii]),int((yy[ii]))),
+                                cv2.FONT_HERSHEY_PLAIN,2,col)
+            
+            try:
+                pts = np.array([[x_, y_] for x_, y_ in zip(xx,yy)],np.int32)
+                i+=1
+                P.append(pts)
+            except ValueError:
+                pass #temporary
+        """ 
+        #Polylines seems to have some trouble rendering multiple polys for some people
+        for p in P:
+            cv2.polylines(z, [p], False, (255,255,255),1)
+        """
+        #hack-y alternative:
+        for p in P:
+            for i in range(len(p)-1):
+                cv2.line(z,tuple(p[i]),tuple(p[i+1]), (255,255,255),1)    
+        cv2.imshow(name,z)
+        cv2.waitKey(1)
+
+
+    def make_plot(self):
+        plotXY([[self.times, self.samples],
+                [self.freqs, self.fft]],
+               labels=[False, True],
+               showmax=[False, "bpm"],
+               label_ndigits=[0, 0],
+               showmax_digits=[0, 1],
+               skip=[3, 3],
+               name=self.plot_title,
+               bg=self.slices[0])
+
+
     def shift(self, detected):
         x, y, w, h = detected
         center = np.array([x + 0.5 * w, y + 0.5 * h])
