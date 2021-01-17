44fd7be57cef44b17fb2168ec4419e42a39656c0
Gregory Tarr <greg@tarr.ie>
2021-01-17 02:10:55 +0000

    removed things that are in dynamic-app

--- app.py
+++ app.py
@@ -17,34 +17,9 @@ from streamlit_webrtc import ClientSettings, VideoTransformerBase, WebRtcMode, w
 
 HERE = Path(__file__).parent
 
-KEY_CONTEXT = st.report_thread.REPORT_CONTEXT_ATTR_NAME
-main_context = getattr(current_thread(), KEY_CONTEXT)
-
 def main():
     st.header("Pulse detector demo")
-    source = pd.DataFrame({
-        'x': np.arange(100),
-        'y': np.zeros(100)
-    })
-    bars = alt.Chart(source).mark_line().encode(
-    x=alt.X('x',axis=alt.Axis(title='Time (s)')),
-    y=alt.Y('y',axis=alt.Axis(title='BPM'))
-    ).properties(width=650, height=400)
-
-    bar_plot = st.altair_chart(bars)
-
     get_pulsemonitor_frames()
-
-def plot_bpm(bpm):
-    x = np.arange(min([100, len(bpm)]))
-    y = bpm[-100:] if len(bpm)>100 else bpm
-    source = pd.DataFrame({
-        'x': x,
-        'y': y
-    })
-    return alt.Chart(source).mark_line().encode(
-        x=alt.X('x',axis=alt.Axis(title='Time (s)'), 
-        y=alt.Y('y',axis=alt.Axis(title='BPM')))).properties(width=650, height=400)
 
 def get_pulsemonitor_frames():
     class NNVideoTransformer(VideoTransformerBase):
@@ -56,16 +31,9 @@ def get_pulsemonitor_frames():
             try:
                 image = frame.to_ndarray(format="bgr24")
                 annotated_image, current_bpm = self.processor.process_frame(image)
-                try:
-                    self.bpm.append(current_bpm)
-                    if len(self.bpm) % 10 == 0:
-                        bars = plot_bpm(self.bpm)
-                        bar_plot.altair_chart(bars)
-                except Exception as e:
-                    traceback.print_exc()
-                    return annotated_image
                 return annotated_image
             except Exception as e:
+                print("Caught Exception")
                 traceback.print_exc()
                 return image
     webrtc_ctx = webrtc_streamer(key="loopback", mode=WebRtcMode.SENDRECV, client_settings=WEBRTC_CLIENT_SETTINGS, video_transformer_factory=NNVideoTransformer, async_transform=True,)
